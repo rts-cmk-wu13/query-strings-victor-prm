be6defde0d985759c2dcb3a6ef3ad7eed8ca4b71
victor-prm <victorpabb@gmail.com>
2025-02-06 22:59:38 +0100

    now fetching object at id parameter in detail view

--- detail.js
+++ detail.js
@@ -1,5 +1,7 @@
 let searchParams = new URLSearchParams(window.location.search)
-console.log(searchParams);
-
+//console.log(searchParams);
 let getID = searchParams.get("id");
-console.log(getID);
+//console.log(getID);
+fetch(`./data/${getID}.json`)
+    .then(res => res.json())
+    .then(data => console.log(data));
